6dd3ee435523bd72c70edf4842765cf435ddb647
beranon1 <beranon1@uhk.cz>
2024-09-29 17:06:48 +0200

    start

---
+++ src/test/java/com/example/ppro_projekt/PproProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ppro_projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PproProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
